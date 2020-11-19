2bb2a6170b81fd706f3df9a9d96e92d7cf2f7e9b
Eric <eric.srna@gmail.com>
2020-11-19 18:50:21 -0500

    Add Camera, character movement, Colliders - Sinan

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float moveSpeed = 5f;
     // Start is called before the first frame update
+    SpriteRenderer sRenderer;
     void Start()
     {
-        
+        sRenderer = GetComponent<SpriteRenderer>();        
     }
 
     // Update is called once per frame
@@ -24,5 +25,13 @@ public class CharacterController : MonoBehaviour
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         var newYPos = transform.position.y + deltaY;
         transform.position = new Vector2(newXPos, newYPos);
+        if(deltaX < 0)
+        {
+            sRenderer.flipX = true;
+        }
+        else if(deltaX> 0)
+        {
+            sRenderer.flipX = false;
+        }
     }
 }
