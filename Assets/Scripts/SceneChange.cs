746288b03d6effe7f53f2c5df715ef5c5f376c50
Eric <eric.srna@gmail.com>
2020-11-25 21:24:25 -0500

    Add Sample UI

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void ChangeFirstScene()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void ChangeSecondScene()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+}
