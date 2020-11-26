a66ed94db5e046e480a7d071e000f1600a13da0f
Eric <eric.srna@gmail.com>
2020-11-25 19:45:36 -0500

    Simple Enemy movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] int moveRange = 500;
+    float movePixelX = 2f;
+    int count = 0;
+    bool flip = false;
+    Rigidbody2D myRigidBody;
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(flip.Equals(false))
+        {
+            myRigidBody.velocity = new Vector2(movePixelX, 0f);
+            count++;
+            if(count > moveRange)
+            {
+                flip = true;
+                movePixelX = -2f;
+            }
+        } else {
+            myRigidBody.velocity = new Vector2(movePixelX, 0f);
+            count--;
+            if (count < -moveRange)
+            {
+                flip = false;
+                movePixelX = 2f;
+            }
+        }
+    }
+}
